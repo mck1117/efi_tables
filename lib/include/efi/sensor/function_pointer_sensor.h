542d20cc2214f31159e308b78edf342c5641d7e7
Matthew Kennedy <matthewkennedy@outlook.com>
2019-08-23 18:34:58 -0700

    comment

--- lib/include/efi/sensor/function_pointer_sensor.h
+++ lib/include/efi/sensor/function_pointer_sensor.h
@@ -1,5 +1,9 @@
 #include <efi/sensor/sensor.h>
 
+/* This class is intended as a bridge to bridge from old getMySensor() functions
+ * to the new system.  This way, producers and consumers can be independently
+ * updated to the new system, with sensors being usable either way for some time.
+ */
 class FunctionPointerSensor final : public Sensor
 {
 public:
